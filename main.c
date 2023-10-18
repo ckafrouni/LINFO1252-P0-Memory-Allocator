16154b0544093846560b4045adb117c2f5ca5d78
Christophe Kafrouni <ckafrouni@proton.me>
2023-10-18 19:23:00 +0000

    Refactorisation de malloc

--- main.c
+++ main.c
@@ -4,38 +4,47 @@
 
 #include "my_malloc.h"
 
+/**
+ * Prints the state of the heap
+ *
+ * The heap consists of a series of blocks, each of which has a header and a payload.
+ * The header is 2 bytes long, and contains the size of the block (in bytes).
+ * The payload is the actual data that is stored in the block.
+ *
+ * The first 2 bytes of the heap (aka: <S>) is an offset to the first free block.
+ *
+ * The first 2 bytes of each block (aka: <H>) is the size of the block (in bytes).
+ *
+ * The list of free blocks is a linked list, where each block contains the offset to the next free block.
+ * The offset is the number of bytes between the two free blocks.
+ */
 void print_all_blocks()
 {
-    const uint16_t METADATA_SIZE = 2;
-    uint16_t *start = (uint16_t *)MY_HEAP;
-    uint16_t *current = (uint16_t *)(MY_HEAP + METADATA_SIZE); // Skip the initial "start" pointer
+    // const uint16_t METADATA_SIZE = 2;
+    uint16_t *s = (uint16_t *)MY_HEAP;
+    uint16_t *current = (uint16_t *)(MY_HEAP + *s);
 
     printf("\033[34;1m=== HEAP STATE ===\n\033[0;1m");
     printf("heap address = %p\n", MY_HEAP);
-    printf("1st free block's offset = %u\n", *start);
+    printf("1st free block's offset = %u\n", *s);
     printf("\033[34m------------------\n");
 
-    int num_block = 0;
+    int n = 0;
     while ((uint8_t *)current < MY_HEAP + sizeof(MY_HEAP))
     {
-        uint16_t size = *current & ~1; // Mask out the lowest bit to get the size
-        uint16_t second_size = *(current + (size / 2) - 1) & ~1;
-        int is_allocated = *current & 1; // Check the lowest bit to see if it's allocated
-        uint16_t offset = (uint8_t *)current - (uint8_t *)MY_HEAP;
+        uint16_t size = *current; // Mask out the lowest bit to get the size
+        uint16_t offset_from_start = (uint8_t *)current - (uint8_t *)MY_HEAP;
+        uint16_t next_offset = *(current + 1);
 
-        printf("\033[31m#%d\033[0;1m\t", num_block);
-        if (is_allocated)
-        {
-            printf("\033[33mallocated\toffset = %u\t%%p = %p\tsize = ( %u - %u )\tcontent: \"%s\"\n\033[0;1m", offset, current, size, second_size, (char *)(current + 1));
-        }
-        else
-        {
-            uint16_t next_offset = *(current + 1);
-            printf("\033[32mfree\t\toffset = %u\t%%p = %p\tsize = ( %u - %u )\tnext offset = %u\n\033[0;1m", offset, current, size, second_size, next_offset);
-        }
-
-        num_block++;
-        current += size / METADATA_SIZE; // Move to the next block based on the size
+        printf("\033[31m#%d\033[0;1m\t", n);
+        printf("\033[32mfree\t");
+        printf("\033[33moffset from start =\033[0;1m %u\t", offset_from_start);
+        printf("\033[33m%%p =\033[0;1m %p\t", current);
+        printf("\033[33msize =\033[0;1m %u\t", size);
+        printf("\033[33mnext offset =\033[0;1m %u\n", next_offset);
+        
+        n++;
+        current = (uint16_t *)((uint8_t *)current + size); // Move to the next block based on the size
     }
 
     printf("\033[34m==================\n\n\033[0m");
@@ -68,22 +77,22 @@ int main()
     printf("my_free(p);\n");
     print_all_blocks();
 
-    my_free(q);
-    printf("my_free(q);\n");
-    print_all_blocks();
+    // my_free(q);
+    // printf("my_free(q);\n");
+    // print_all_blocks();
 
-    p = (char *)my_malloc(6);
-    strcpy(p, "Hello");
-    printf("char *p = (char *)my_malloc(6);\nstrcpy(p, \"Hello\");\n");
-    print_all_blocks();
+    // p = (char *)my_malloc(6);
+    // strcpy(p, "Hello");
+    // printf("char *p = (char *)my_malloc(6);\nstrcpy(p, \"Hello\");\n");
+    // print_all_blocks();
 
-    q = (char *)my_malloc(7);
-    strcpy(q, "world!");
-    printf("q = (char *)my_malloc(7);\nstrcpy(q, \"world!\");\n");
-    print_all_blocks();
+    // q = (char *)my_malloc(7);
+    // strcpy(q, "world!");
+    // printf("q = (char *)my_malloc(7);\nstrcpy(q, \"world!\");\n");
+    // print_all_blocks();
 
-    char *a = (char *) my_malloc(10);
-    print_all_blocks();
+    // char *a = (char *)my_malloc(10);
+    // print_all_blocks();
 
     // my_free(pq);
     // printf("my_free(pq);\n");
--- my_malloc.c
+++ my_malloc.c
@@ -18,25 +18,18 @@ void my_init()
 
     *start = METADATA_SIZE; // offset to first free block
 
-    // TODO : Il ne faut plus prendre en compte le footer
-    // TODO : plus besoin de bit de poids faible.
-    
     uint16_t block_size = HEAP_SIZE - 1 * METADATA_SIZE;
-    *(block + 0) = block_size;                    // block size (in bytes)
-    *(block + 1) = 0;                             // offset to next free block (0)
-    *(block + (block_size / 2) - 1) = block_size; // last: = 1st
+    *(block + 0) = block_size; // block size (in bytes)
+    *(block + 1) = 0;          // offset to next free block (0)
 }
 
 void *my_malloc(size_t size)
 {
     const uint16_t METADATA_SIZE = 2;
-    const uint16_t MIN_BLOCK_SIZE = 3 * METADATA_SIZE;
-    // TODO : plus besoin de bit de poids faible.
+    const uint16_t MIN_BLOCK_SIZE = 2 * METADATA_SIZE; // header, and next offset
 
-    // TODO : la taille impair est acceptée, ainsi que plus besoin de footer.
-    size += 2 * METADATA_SIZE; // Additional words for metadata
+    size += 1 * METADATA_SIZE; // Additional words for header
     size = size < MIN_BLOCK_SIZE ? MIN_BLOCK_SIZE : size;
-    size = (size + 1) & ~1; // rounding to nearest even size
 
     uint16_t *start = (uint16_t *)MY_HEAP;
     uint16_t *current = (uint16_t *)(MY_HEAP + *start);
@@ -44,7 +37,7 @@ void *my_malloc(size_t size)
 
     while (current != NULL)
     {
-        uint16_t block_size = *(current + 0);
+        uint16_t block_size = *current;
         if (block_size >= size)
         {
             uint16_t remaining_size = block_size - size;
@@ -52,37 +45,24 @@ void *my_malloc(size_t size)
 
             if (remaining_size >= MIN_BLOCK_SIZE)
             {
-                // TODO : Il ne faut plus prendre en compte le footer
                 // Split block
-                new_block = current + (size / 2);
-                *(new_block + 0) = remaining_size;
+                new_block = (uint16_t *)((uint8_t *)current + size);
+                *new_block = remaining_size;
                 *(new_block + 1) = *(current + 1) == 0 ? 0 : *(current + 1) - size; // offset to next free block
-                *(new_block + (remaining_size / 2) - 1) = remaining_size;
-            }
-            else if (remaining_size != 0)
-            {
-                // Create unusable block, to be reclaimed by free later
-                uint16_t *block = current + (size / 2);
-                *(block + 0) = remaining_size;
-                if (remaining_size != 2)
-                {
-                    *(block + (remaining_size / 2) - 1) = remaining_size;
-                }
             }
 
             if (prev == NULL)
             {
+                // (prev == NULL) only at the first iteration
                 *start = new_block == NULL ? *start + *(current + 1) : (uint8_t *)new_block - MY_HEAP; // offset from MY_HEAP
             }
             else
             {
-                *(prev + 1) = new_block == NULL ? *(current + 1) : (uint8_t *)new_block - (uint8_t *)prev;
+                *(prev + 1) = new_block == NULL ? *(prev + 1) + *(current + 1) : (uint8_t *)new_block - (uint8_t *)prev;
             }
 
-            // TODO : Il ne faut plus prendre en compte le footer
             // Update current block
-            *(current + 0) = size | 1;
-            *(current + (size / 2) - 1) = size | 1;
+            *current = size;
 
             return (void *)(current + 1);
         }
@@ -94,7 +74,7 @@ void *my_malloc(size_t size)
     return NULL;
 }
 
-void my_free(void *pointer)
+void _my_free(void *pointer)
 {
     const uint16_t HEAP_SIZE = 64000;
 
@@ -151,7 +131,8 @@ void my_free(void *pointer)
     *(ptr + 1) = current == NULL ? 0 : (uint8_t *)current - (uint8_t *)ptr;
 }
 
-void my_free2 (void* pointer) {
+void my_free(void *pointer)
+{
     // Inginious constant
     const uint16_t HEAP_SIZE = 64000;
 
@@ -163,13 +144,16 @@ void my_free2 (void* pointer) {
 
     // Calculate offset of the element to free
     uint16_t offset = (uint8_t *)ptr_old_block - (uint8_t *)start;
-    if (offset > HEAP_SIZE) return -1;
+    if (offset > HEAP_SIZE)
+        return;
 
     // Find previous and next free elements
-    uint16_t* ptr_previous_free = (uint16_t *)1;
-    uint16_t* ptr_next_free = (uint16_t *)start;
-    do {
-        if ((ptr_previous_free - start) < offset && offset < (ptr_next_free - start)) break;
+    uint16_t *ptr_previous_free = (uint16_t *)1;
+    uint16_t *ptr_next_free = (uint16_t *)((uint8_t *)start + *start);
+    do
+    {
+        if ((ptr_previous_free - start) < offset && offset < (ptr_next_free - start))
+            break;
         ptr_previous_free = ptr_next_free;
         ptr_next_free = (uint16_t *)*ptr_next_free;
     } while (*ptr_next_free != 0);
@@ -178,7 +162,8 @@ void my_free2 (void* pointer) {
     *(ptr_old_block + 1) = *ptr_next_free;
 
     // Is merger with next required ?
-    if (offset + *(ptr_old_block) == ((uint8_t *)ptr_next_free - (uint8_t *)start)){
+    if (offset + *(ptr_old_block) == ((uint8_t *)ptr_next_free - (uint8_t *)start))
+    {
         // Add next_free size to old size
         *ptr_old_block += *ptr_next_free;
         // Old point to next of next
@@ -186,12 +171,15 @@ void my_free2 (void* pointer) {
     }
 
     // Is merger with previous required ?
-    if ((((uint8_t *)ptr_previous_free - (uint8_t *)start) + *(ptr_previous_free)) == offset) {
+    if ((((uint8_t *)ptr_previous_free - (uint8_t *)start) + *(ptr_previous_free)) == offset)
+    {
         // Add old size to previous_free size
         *(ptr_previous_free) += *(ptr_old_block);
         // Point to the next of next_free if there was a merger with the next_free
         *(ptr_previous_free + 1) = *(ptr_old_block + 1);
-    } else {
+    }
+    else
+    {
         *(ptr_previous_free + 1) = offset;
     }
 }
